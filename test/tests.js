eeca647df90f7d389b10f1785dae25aee714b6ba
Jordan Harband <ljharb@gmail.com>
2025-02-25 16:32:17 -0800

    [Tests] add tests from https://github.com/tc39/test262/pull/4390

--- test/tests.js
+++ test/tests.js
@@ -3,6 +3,7 @@
 var functionsHaveNames = require('functions-have-names')();
 var boundFunctionsHaveNames = require('functions-have-names').boundFunctionsHaveNames();
 var hasPropertyDescriptors = require('has-property-descriptors')();
+var hasSymbols = require('has-symbols/shams')();
 
 module.exports = function (SuppressedError, t) {
 	t.test('constructor', function (st) {
@@ -62,6 +63,143 @@ module.exports = function (SuppressedError, t) {
 
 		st.equal(error.error, one, 'error.error is provided cause');
 		st.equal(error.suppressed, two, 'error.suppressed is provided suppressed error');
+
+		st.end();
+	});
+
+	t.test('test262: test/built-ins/NativeErrors/SuppressedError/message-method-prop-cast', function (st) {
+		/* eslint no-restricted-syntax: 0 */
+
+		var case1 = new SuppressedError(undefined, undefined, 42);
+		st.equal(case1.message, '42', 'message is "42"');
+		for (var k in case1) { st.fail(k + ' is enumerable'); }
+
+		var case2 = new SuppressedError(undefined, undefined, false);
+		st.equal(case2.message, 'false', 'message is "false"');
+		for (k in case2) { st.fail(k + ' is enumerable'); }
+
+		var case3 = new SuppressedError(undefined, undefined, true);
+		st.equal(case3.message, 'true', 'message is "true"');
+		for (k in case3) { st.fail(k + ' is enumerable'); }
+
+		var case4 = new SuppressedError(undefined, undefined, { toString: function () { return 'string'; } });
+		st.equal(case4.message, 'string', 'message is "string"');
+		for (k in case4) { st.fail(k + ' is enumerable'); }
+
+		var case5 = new SuppressedError(undefined, undefined, null);
+		st.equal(case5.message, 'null', 'message is "null"');
+		for (k in case5) { st.fail(k + ' is enumerable'); }
+
+		st.end();
+	});
+
+	t.test('test262: test/built-ins/NativeErrors/SuppressedError/message-method-prop', function (st) {
+		/* eslint no-restricted-syntax: 0 */
+
+		var obj = new SuppressedError(undefined, undefined, '42');
+		st.equal(obj.message, '42', 'message is "42"');
+		for (var k in obj) { st.fail(k + ' is enumerable'); }
+
+		st.end();
+	});
+
+	t.test('test262: test/built-ins/NativeErrors/SuppressedError/message-tostring-abrupt-symbol', { skip: !hasSymbols || !Symbol.toPrimitive }, function (st) {
+		var case1 = Symbol();
+
+		st['throws'](function () { return new SuppressedError(undefined, undefined, case1); }, TypeError, 'toPrimitive');
+
+		var case2 = {
+			toString: function () {
+				throw new EvalError();
+			},
+			valueOf: function () {
+				throw new EvalError();
+			}
+		};
+
+		case2[Symbol.toPrimitive] = function () {
+			return Symbol();
+		};
+
+		st['throws'](function () { return new SuppressedError(undefined, undefined, case2); }, TypeError, 'from ToPrimitive');
+
+		st.end();
+	});
+
+	t.test('test262: test/built-ins/NativeErrors/SuppressedError/message-tostring-abrupt', function (st) {
+		/* eslint no-throw-literal: 0 */
+
+		if (hasSymbols && Symbol.toPrimitive) {
+			var case1 = {
+				toString: function () {
+					throw 'toString called';
+				},
+				valueOf: function () {
+					throw 'valueOf called';
+				}
+			};
+			case1[Symbol.toPrimitive] = function () {
+				throw new EvalError();
+			};
+
+			st['throws'](function () { return new SuppressedError(undefined, undefined, case1); }, EvalError, 'toPrimitive');
+		}
+
+		var case2 = {
+			toString: function () {
+				throw new EvalError();
+			},
+			valueOf: function () {
+				throw 'valueOf called';
+			}
+		};
+
+		st['throws'](function () { return new SuppressedError(undefined, undefined, case2); }, EvalError, 'toString');
+
+		var case3 = {
+			toString: undefined,
+			valueOf: function () {
+				throw new EvalError();
+			}
+		};
+
+		st['throws'](function () { return new SuppressedError(undefined, undefined, case3); }, EvalError, 'valueOf');
+
+		st.end();
+	});
+
+	t.test('test262: test/built-ins/NativeErrors/SuppressedError/newtarget-is-undefined', function (st) {
+		var obj = SuppressedError();
+
+		st.ok(Object.prototype.isPrototypeOf.call(SuppressedError.prototype, obj));
+		st.ok(obj instanceof SuppressedError);
+
+		st.end();
+	});
+
+	t.test('test262: test/built-ins/NativeErrors/SuppressedError/order-of-args-evaluation', function (st) {
+		var messageStringified = false;
+		var message = {
+			toString: function () {
+				messageStringified = true;
+				return '';
+			}
+		};
+		var error = {};
+		var suppressed = {};
+
+		var e = new SuppressedError(error, suppressed, message);
+
+		st.equal(messageStringified, true);
+		var keys = Object.getOwnPropertyNames(e);
+		st.deepEqual(keys, ['stack', 'message', 'error', 'suppressed']);
+
+		st.end();
+	});
+
+	t.test('test262: test/built-ins/NativeErrors/SuppressedError/prototype/errors-absent-on-prototype', function (st) {
+		st.equal(Object.prototype.hasOwnProperty.call(SuppressedError.prototype, 'error'), false);
+		st.equal(Object.prototype.hasOwnProperty.call(SuppressedError.prototype, 'suppressed'), false);
 
 		st.end();
 	});
